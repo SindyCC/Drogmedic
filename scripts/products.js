47ea1a73ba81cefc82e6fb8f210f0a66247323f9
Robin <cuentadersab2gmail.com>
2023-09-10 18:32:30 -0500

    saving products in javascript

--- scripts/products.js
+++ scripts/products.js
@@ -1,3 +1,20 @@
-
+const product = [{
+  image:  'img/products_images/advil.jpg',
+  name: 'Advil',
+  price: 33350
+}, 
+{
+  image: 'img/products_images/apronax.jpg',
+  name: 'Apronax',
+  price: 15950
+}, {
+  image: 'img/products_images/aspirina.jpg',
+  name: 'Aspirina',
+  price: 79950
+}, {
+  image: 'img/products_images/digestar_jalea.jpg',
+  name: 'Digestar Jalea',
+  price: 60450
+}];
 
 console.log('Hello World');
